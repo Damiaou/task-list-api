db0a62b7390720642bc217735f1369ed1b0dc151
Dam <damsauvagere@gmail.com>
2021-01-09 11:57:22 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+let pool = null;
+try {
+  pool = new Pool({
+    user: "sjoycbos",
+    host: "kandula.db.elephantsql.com",
+    database: "sjoycbos",
+    password: process.env.pg_password,
+    port: 5432
+  });
+} catch (e) {
+  console.log(e);
+}
+
+// Warning if something went wrong with the db
+if (!pool) {
+  app.get("/", (req, res) => {
+    res.send(
+      "<h2 style='color:red'>Connection with db not working properly :(</h2>"
+    );
+  });
+}
+
+app.listen(8080, () => {
+  console.log("Started on (http://localhost:8080/) !");
+});
+
+// Warm welcome for all users
+app.get("/", (req, res) => {
+  res.send("<h2>Welcome to task list API</h2>");
+});
+
+// First home get and create
+app
+  .route("/home")
+  .get((req, res) => {
+    getHomes(req, res);
+  })
+  .post((req, res) => {
+    createHome(req, res);
+  });
+
+const getHomes = (request, response) => {
+  pool.query("SELECT * FROM home", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createHome = (request, response) => {
+  const { hash, label } = request.body;
+  pool.query(
+    "INSERT INTO home (hash, label) VALUES ($1, $2)",
+    [hash, label],
+    (error, result) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Home added with hash: ${result.hash}`)
+    }
+  );
+};
